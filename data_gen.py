2d65bbefc34b22125121cb7c591a88319edbea14
rajaraju <pgrajaraju@hotmail.com>
2018-10-02 13:48:17 +0530

    modified function arguments

--- data_gen.py
+++ data_gen.py
@@ -1,30 +1,25 @@
 import os, shutil
 
-def dataset_generation(download_dir,base_dir,number_of_classes,class_names):
+DOWNLOAD_DIR = '/Users/rajaraju/Downloads/Birds/'
+BASE_DIR = '/Users/rajaraju/Downloads/birds_classification1'
+
+def dataset_generation(class_labels):
     """
-        Collects data from download location, segregates data in different folders
-        based on object class.
+        Creates the folder structure for Training, Validation and Test data and Class Labels.
 
         Parameters:
-
-        download_dir : Original location of the data downloaded.
-        base_dir : Location of the directory where data will be stored as training and test sets based on their class.
-        number_class : Number of object classes
-        class_names : Names of the classes, for example: cats, dogs, horses.
-
-        Returns : training and test directory.
+        class_labels : Names of the classes, for example: cats, dogs, horses.
 
     """
 
-    os.mkdir(base_dir)
-
     dataset_folders = ['train','validation','test']
-    object_class = ['cat','dog','horse']
-    os.mkdir(base_dir)
+    object_class = class_labels
+    os.mkdir(BASE_DIR)
+
     for folder in dataset_folders:
         for obj_cls in object_class:
-            training_dir = base_dir + os.sep +'{}'.format(folder)
-            if not os.path.exists(base_dir+os.sep +'{}'.format(folder)):
+            training_dir = BASE_DIR + os.sep +'{}'.format(folder)
+            if not os.path.exists(BASE_DIR+os.sep +'{}'.format(folder)):
                 os.mkdir(training_dir)
             class_dir = training_dir + os.sep + '{}'.format(obj_cls)
             if not os.path.exists(training_dir + os.sep + '{}'.format(obj_cls)):
